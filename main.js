e4e0fe5496d25d2d8ab3f4cfdf42cd9c54efe9f8
emashovvn2 <emashovvn2@sibmail.com>
2020-08-20 20:25:26 +0700

    Homework 2

---
+++ main.js
@@ -0,0 +1,6 @@
+function formattedPhone(phone) {
+	
+	const result = phone.charAt(0) + phone.charAt(1) + ' (' + phone.charAt(2) + phone.charAt(3) + phone.charAt(4) + ') ' + phone.charAt(5) + phone.charAt(6) + phone.charAt(7) + '-' + phone.charAt(8) + phone.charAt(9) + '-' + phone.charAt(10) + phone.charAt(11);
+    return result;
+}
+
